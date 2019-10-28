f9c9924e9e78e73c7c80e0c80e169ebcdc9b3cb2
sam0slo <55109191+sam0slo@users.noreply.github.com>
2019-10-28 18:04:44 -0400

    commit

--- Class9/Assets/Scripts/UI/UIManager.cs
+++ Class9/Assets/Scripts/UI/UIManager.cs
@@ -5,8 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    public Animator startButton;
+    public Animator settingsButton;
+    public Animator dialog;
+
     public void StartGame()
     {
         SceneManager.LoadScene("Main");
+    }
+
+    public void OpenSettings()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings()
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
     }
 }
